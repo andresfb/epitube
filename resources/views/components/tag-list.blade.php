92f3469e1dcc345c2b9cad43ba0703083fa99707
Andres Bastidas <andres@bastidas.us>
2025-11-14 22:00:21 -0500

    Adjust `tag-list.blade.php` for improved tag styles and layout consistency.

--- resources/views/components/tag-list.blade.php
+++ resources/views/components/tag-list.blade.php
@@ -2,9 +2,9 @@
     'tags'
 ])
 
-<div id="tag-list" class="mt-8 grid grid-cols-1 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
+<div id="tag-list" class="mt-8 grid grid-cols-1 gap-2 sm:grid-cols-3 md:grid-cols-4 2xl:grid-cols-6">
 @forelse($tags as $tag)
-    <div class="text-xs md:text-sm lg:text-base font-semibold text-gray-900">
+    <div class="text-sm xl:text-base font-semibold text-gray-900">
         <a href="{{ route('tag', ['slug' => $tag->slug]) }}" class="me-1">
             {{ $tag->name }}
         </a>
