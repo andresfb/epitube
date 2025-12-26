33e2dafb9725cb6264c875a3368dfbe8fcdfad0d
Andres Bastidas <andres@bastidas.us>
2025-12-26 13:06:08 -0500

    Simplify and adjust job scheduling in `console.php`
    - Consolidate `ImportVideosJob` into a single daily run at 09:20.
    - Update `ImportVideosPathsJob` to run daily at 13:20 instead of twice daily.

--- routes/console.php
+++ routes/console.php
@@ -18,13 +18,11 @@ Artisan::command('inspire', function (): void {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// 3 times a day (every 8 hours) at 1:20 AM, 9:20 AM, and 5:20 PM
-Schedule::job(app(ImportVideosJob::class))->cron('20 1,9,17 * * *');
-// 2 times a day at 5:20 AM and 1:20 PM
-Schedule::job(app(ImportVideosPathsJob::class))->cron('20 5,13 * * *');
+Schedule::job(app(ImportVideosJob::class))->dailyAt('09:20');
 Schedule::job(app(ImportRelatedVideosJob::class))->dailyAt('01:45');
 Schedule::job(app(CreateFeedJob::class))->dailyAt('03:25');
 Schedule::job(app(DeleteDisabledJob::class))->dailyAt('05:35');
+Schedule::job(app(ImportVideosPathsJob::class))->dailyAt('13:20');
 Schedule::job(app(UpdateFeedJob::class))->dailyAt('23:05');
 Schedule::job(app(CheckEncodingErrorsJob::class))->dailyAt('23:45');
 Schedule::job(app(ClearTemporaryDisksJob::class))->dailyAt('23:55');
